91b87d06db1a0985ef105685b739356b8dffc698
Epris HB <erichardson@iamcreativeteam.com>
2022-03-01 16:32:31 -0500

    Added sidebar

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,13 @@
 import Pageloader from "../../components/Pageloader"
+import Sidebar from "../../components/Sidebar"
 
 function Home() {
 	return (
 		<div>
 			Home
 			<Pageloader />
+			<div className="app-overlay is-sidebar-v1"></div>
+			<Sidebar />
 		</div>
 	)
 }
