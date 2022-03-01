c078d485f52800fdeea58bfbeb0da785249d5bf5
Epris HB <erichardson@iamcreativeteam.com>
2022-03-01 17:41:19 -0500

    Moved layout structure to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import Home from './pages/Home/Home';
+import Pageloader from "./components/Pageloader";
+import Sidebar from "./components/Sidebar";
+import ChatWrapper from "./components/ChatWrapper";
 
 function App() {
   return (
     <div>
-			<Home />
-		</div>
+      <Pageloader />
+      <div className="app-overlay is-sidebar-v1"></div>
+      <Sidebar />
+      <div className="view-wrapper is-sidebar-v1 is-fold">Home</div>
+      <ChatWrapper />
+    </div>
   );
 }
 
