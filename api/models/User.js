d659ad49f543c084e80822aaa89d8aee11808af3
Epris HB <erichardson@iamcreativeteam.com>
2022-03-08 18:21:57 -0500

    Updated User Schema

--- api/models/User.js
+++ api/models/User.js
@@ -2,63 +2,71 @@ const mongoose = require("mongoose");
 
 // User Schema
 const UserSchema = new mongoose.Schema(
-	{
-		username: {
-			type: String,
-			require: true,
-			min: 3,
-			max: 20,
-			unique: true
-		},
-		email: {
-			type: String,
-			require: true,
-			max: 50,
-			unique: true
-		},
-		password: {
-			type: String,
-			required: true,
-			min: 6
-		},
-		profilePicture: {
-			type: String,
-			default: ''
-		},
-		coverPicture: {
-			type: String,
-			default: ''
-		},
-		followers: {
-			type: Array,
-			default: []
-		},
-		following: {
-			type: Array,
-			default: []
-		},
-		isAdmin: {
-			type: Boolean,
-			default: false
-		},
-		desc: {
-			type: String,
-			max: 50
-		},
-		city: {
-			type: String,
-			max: 50
-		},
-		from: {
-			type: String,
-			max: 50
-		},
-		relationship: {
-			type: Number,
-			enum: [1, 2, 3]
-		}
-	},
-	{timestamps: true}
-)
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 30,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    position: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      max: 50,
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    commonFriends: {
+      type: Number,
+      default: 0,
+    },
+    firstFriend: {
+      type: String,
+      default: "",
+    },
+    secondFrienc: {
+      type: String,
+      default: "",
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
