33769c3299d1c6a5a7b73d98600e54b4771c2266
Epris HB <erichardson@iamcreativeteam.com>
2022-03-09 17:19:36 -0500

    Sort Posts

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -18,7 +18,11 @@ const Feed = forwardRef((props, ref) => {
 	useEffect(() => {
 		const fetchPosts = async () => {
 			const res = await axios.get("api/posts/timeline/" + user._id);
-			setPosts(res.data);
+			setPosts(
+				res.data.sort((p1,p2) => {
+					return new Date(p2.createdAt) - new Date(p1.createdAt);
+				})
+			);
 		};
 		fetchPosts();
 	}, [user._id])
